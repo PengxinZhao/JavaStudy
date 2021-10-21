9608f9a647cd71e4cb682515c26d86e0d5b93725
Pengxin Zhao <1359280299@qq.com>
2021-10-21 23:00:08 +0800

    标识符

--- JavaSE/day02/src/java/HelloWorld.java
+++ JavaSE/day02/src/java/HelloWorld.java
@@ -8,10 +8,15 @@ public class HelloWorld {
      * static表示静态的
      * void表示空
      * main表示方法名
-     *
+     *(String[] args) 是一个main方法的形式参数列表
+     * 需要记住的是：
+     *      以下的方法是一个程序的“主方法”，是程序的执行入口
+     *      是sun公司规定的，固定编写方式
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {//表示顶一个公开的静态的主方法
         System.out.println("Hello World!");
+
+        System.out.println("哈喽世界");
     }
 }
---
+++ JavaSE/day02/src/java/IdentifierTest01.java
@@ -0,0 +1,20 @@
+
+/*
+        关于java语言当中的标识符
+            1、什么是标识符？
+                - 在java源程序中凡是程序员有权利自己明明的单词都是标识符
+                - 标识符在EditPlus编辑器中以黑色字体高亮显示
+                - 标识符可以标识什么元素呢？
+                    * 类名
+                    * 方法名
+                    * 接口名
+                    * 常量名
+            2、标识符的命名规则？
+
+            3、标识符的命名规范？
+ */
+public class IdentifierTest01 {
+    public static void main(String[] args) {
+
+    }
+}
