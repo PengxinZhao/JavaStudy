17acf26d12d1ca7d5aa1ee7bb057093822e327ff
Pengxin Zhao <1359280299@qq.com>
2021-10-21 22:30:41 +0800

    HelloWorld

---
+++ JavaSE/day02/src/java/HelloWorld.java
@@ -0,0 +1,17 @@
+//public表示公开
+//class表示定义一个类
+//HelloWorld表示一个类名
+public class HelloWorld {
+
+    /**
+     * public表示公开的
+     * static表示静态的
+     * void表示空
+     * main表示方法名
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
